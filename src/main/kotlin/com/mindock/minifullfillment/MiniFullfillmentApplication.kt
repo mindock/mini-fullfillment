0625a996938d342ef282dfd49c74aef062e05aa9
MINDOCK <parksy8189@gmail.com>
2022-07-07 23:06:11 +0900

    🎉 mini fullfillment service init

---
+++ src/main/kotlin/com/mindock/minifullfillment/MiniFullfillmentApplication.kt
@@ -0,0 +1,11 @@
+package com.mindock.minifullfillment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MiniFullfillmentApplication
+
+fun main(args: Array<String>) {
+    runApplication<MiniFullfillmentApplication>(*args)
+}
---
+++ src/test/kotlin/com/mindock/minifullfillment/MiniFullfillmentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mindock.minifullfillment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MiniFullfillmentApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
